40793145c89cae49c19c366b1d0196112ed040a8
Sungwoo Kim <paulkim9292@gmail.com>
2022-07-01 18:36:34 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+//import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+import Movie from "./components/Movie";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route
+          path={`${process.env.PUBLIC_URL}/movie/:id`}
+          element={<Detail />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
